ba1b852ba1910e8b82697cc12f4a7e71850aaa46
savvz <55504101+savvz@users.noreply.github.com>
2020-04-20 15:27:08 +0530

    Added patient per cluster calculations
    Added functions to decide patient count per cluster. Used regression to assign weights to population intensity colors, and used these to predict total count

--- ZoneController/ZoneController.py
+++ ZoneController/ZoneController.py
@@ -29,12 +29,15 @@ class ZoneController:
 		self.location_coordinates=[]
 		self.patient_data=[]
 		self.location_clusters=[]
-		self.clusters_details=[]
+		self.clusters_details={}
 		self.clustering_algorithm_parameters={'quantile': .1,
 											'eps': .0095,
 											'damping': .98,
 											'n_clusters': 10}
 		self.clustering_algorithms=['DBSCAN','MeanShift','AffinityPropagation','KMeans']
+		self.wards=[]
+		self.population_intensity=[]
+		self.population_intensity_weights=[]
 
 	# function getClusteringObject(cluster_algo):
 	# 	input: clustering algorithm to use
@@ -99,16 +102,121 @@ class ZoneController:
 		
 		return assigned_clusters
 
+	# function generate_A(data,path to ward neighbors file):
+	# 	input: given data having latitude longitude assigned with ward ids and corresponding population intensity
+	# 	output: A matrix to be used for weights computation
+	# 	Description:
+	# 	The function is used to compile matrix A that is used furthen in weights computation. The formulation is Ax=b 
+	# 	to be used to fit a given cluster data according to three variables for population intensity.
+
+	def generate_A(self,X,path_data):
+		A_rows=len(self.wards)
+		A_columns=len(self.population_intensity)
+		A=np.zeros((A_rows,A_columns))
+		missing_wards=[]
+		for ww in range(A_rows):
+			if self.wards[ww] in X["ward_id"].unique():
+				for cc in range(A_columns):
+					A[ww,cc]=len(X[(X["ward_id"]==self.wards[ww]) & (X["color_marker"]==self.population_intensity[cc])])
+			else:
+				missing_wards.append(ww)
+		#for missing wards fill the details by averaging neighboring wards
+		if len(missing_wards)>0:
+			wards_neighbors=pd.read_csv(path_data)
+			for mw in missing_wards:
+				mw_neighbors=wards_neighbors[wards_neighbors["ward_name"]==self.wards[mw]]["neighbours"].values[0].replace("[","").replace("]","").split(",")
+				for cc in range(A_columns):
+					for mwn in mw_neighbors:
+						A[mw,cc]+=len(X[(X["ward_id"]==mwn) & (X["color_marker"]==self.population_intensity[cc])])
+					A[mw,cc]= np.ceil(A[mw,cc]/len(mw_neighbors))
+		return A
+
+	# function get_b(data ,date):
+	# 	input: data with ward and intensity information
+	#		   date for which the count has to be considered to fit the model
+	# 	output: b vector describing total cases in every word
+	# 	Description:
+	#	This function is used to create the b vector needed for solving Ax=b
+
+	def get_b(self,X,date_string):
+		b=np.zeros((len(self.wards)))
+		for ww in range(len(self.wards)):
+			b[ww]=X[X["ward_name"]==self.wards[ww]][date_string].values[0]
+		return b
+
+	# function get_weights(Amatrix ,b vector):
+	# 	input: A matrix depicting count of cases for every population intensity in every ward,
+	#		   b vector depicting total no of cases per ward
+	# 	output: weight vector for population intensity
+	# 	Description:
+	# 	The function is used to find the weights for different population intensity. There are different population intensity which 
+	#	are grouped as "Red": for congested areas, "Orange" : medium level congested area  and "Blue": stanalone structure depicting
+	#	very less crowd.
+	#	Given total number of cases are combined for all these three. We find the weights in which these areas contribute to final count
+	
+	def get_weights(self,X,b):
+		Xt=np.linalg.inv(np.matmul(X.T,X))
+		weights=np.matmul(np.matmul(Xt,X.T),b)
+		return weights
+
+
+	# function get_cases(data ):
+	# 	input: data with cluster and intensity assignment
+	# 	output: matrix filled with no of cases per cluster per population intensity
+	# 	Description:
+	#	This function is used to find patient distribution in every cluster and use weights to find total cases in cluster
+
+	def get_cases(self,X):
+		clusters=X["cluster_assignment"].unique()
+		cluster_cases=np.zeros((len(clusters),len(self.population_intensity)))
+		for cl in range(len(clusters)):
+			for cc in range(len(self.population_intensity)):
+				cluster_cases[cl,cc]=len(X[(X["cluster_assignment"]==clusters[cl]) & (X["color_marker"]==self.population_intensity[cc])])
+		return cluster_cases
+
 if ( __name__ == "__main__"):
 	zone_object=ZoneController()
 
+
+	#hard-coded values or input to be asked to user
 	location_data_path="../MLData/Mumbai/mumbai_combined.csv"
+	patient_data_path="../MLData/Mumbai/ward_wise_positive_cases.csv"
+	neighbours_data_path="../MLData/Mumbai/ward_neighbors.csv"
+	date_to_filter="14.04.20"
+	clustering_algorithm_to_use="DBSCAN"
+
+	#location data for clustering
 	location_data=pd.read_csv(location_data_path)
 	zone_object.location_coordinates=location_data[["latitude","longitude"]].values
 
-
-	patient_data_path="../MLData/Mumbai/positive_case.csv"
+	#total patient data 
 	zone_object.patient_data=pd.read_csv(patient_data_path)
 
-	zone_object.location_clusters=zone_object.createClusters("DBSCAN")
-	zone_object.plotClusters("DBSCAN")
+	zone_object.location_clusters=zone_object.createClusters(clustering_algorithm_to_use)
+	zone_object.plotClusters(clustering_algorithm_to_use)
+	
+	#assign patient data to clusters along with population in the cluster.
+	zone_object.wards=zone_object.patient_data["ward_name"].unique()
+	zone_object.population_intensity=["Red","Orange","Blue"]
+	
+	#Regression in three dimensions (Red,Blue,Orange)
+	#variables are weights assigned to every dimension w1,w2,w3
+	#the formulation becomes of the form Ax=b 
+	#where A is matrix with counts for red,blue and orange for each ward
+	#b is total cases in the ward
+	#x is vector w1,w2 and w3
+	#we generate A matrix, if transpose(A)*A is invertible we can directly solve this using closed form solution for x
+	A_mat=zone_object.generate_A(location_data,neighbours_data_path)
+	b_vec=zone_object.get_b(zone_object.patient_data,date_to_filter)
+	weights=zone_object.get_weights(A_mat,b_vec)
+	print("Weights for population intensity:\n",weights)
+
+	#find case distribution in every cluster and use weights to find total cases in cluster
+	location_data["cluster_assignment"]=zone_object.location_clusters[:,2]
+	clusters=np.unique(zone_object.location_clusters[:,2])
+	print("clusters:\n",clusters)
+	cases_per_cluster_per_intensity=zone_object.get_cases(location_data)
+	print("Matrix depicted cases per cluster per intensity:\n",cases_per_cluster_per_intensity)
+	# #predict total cases per cluster using weights of intensity colors
+	total_cases_per_cluster=np.round(np.matmul(cases_per_cluster_per_intensity,weights))
+	print("Total cases per cluster:\n",total_cases_per_cluster)
